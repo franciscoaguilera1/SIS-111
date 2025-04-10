d60b05733961e521a4e951b8422d8dfed962b383
franciscoaguilera1 <francisco.aguilera@ucb.edu.bo>
2025-04-10 09:50:07 -0400

    Primer codigo en python operadores en aritmetica

---
+++ Operaciones_aritmeticas.py
@@ -0,0 +1,28 @@
+
+#Suma de numeros
+num1 = 10
+num2 = 20
+Suma = num1 + num2
+print(Suma)
+
+#Suma de palabras -> conectar palabras
+palabra1 = "Hola "
+palabra2 = "mundo"
+suma_palabra = palabra1 + palabra2
+print(suma_palabra)
+#Suma y resta
+num3 = 0
+num4 = 10
+num5 = 15
+num6 = 20
+num7 = 25
+num8 = 30
+suma1 = num3 + num4
+resta = num7 - num4
+multiplicacion = num5 * num6
+division = num8 / num4
+print(suma1)
+print(resta)
+print(multiplicacion)
+print(division)
+print()
